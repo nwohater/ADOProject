f35d3c704de9b4e5e91e30795208cab26c53b24e
Brandon Lackey <bslackey@gmail.com>
2022-11-16 15:17:49 -0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using ADOProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using LayerDAL.Repository;
+using LayerDAL.Entities;
+
+namespace ADOProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _repository;
+
+        public HomeController(IEmployeeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> ShowEmployees()
+        {
+            List<Employee> ListEmployees = await _repository.GetEmployees();
+            return View(ListEmployees);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADOProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
